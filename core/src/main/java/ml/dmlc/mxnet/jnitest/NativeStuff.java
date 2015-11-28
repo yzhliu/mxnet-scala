f8af1c802a7b886f2d8c5c1bed57bd8e41992e41
Yizhi Liu <javelinjs@gmail.com>
2015-11-28 17:49:05 +0800

    toy pom settings for jni. todo: -c param, library loading error

---
+++ core/src/main/java/ml/dmlc/mxnet/jnitest/NativeStuff.java
@@ -0,0 +1,9 @@
+package ml.dmlc.mxnet.jnitest;
+
+public class NativeStuff {
+  public native void helloNative();
+  public static void main(String[] args) {
+    System.load("/Users/lewis/Workspace/ml/mxnet-scala/native/osx-x86_64/target/mxnet-scala-native.so");
+    new NativeStuff().helloNative();
+  }
+}
