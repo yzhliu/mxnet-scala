65de79e3ce835acd965c4d6a26198d3c48c54962
terrytangyuan <terrytangyuan@gmail.com>
2015-12-10 23:20:11 -0600

    Added deps scalatest

---
+++ core/src/test/scala/TestSpec.scala
@@ -0,0 +1,16 @@
+/**
+ * Created by yuantang on 12/10/15.
+ */
+package ml.dmlc.mxnet
+
+import org.scalatest.__
+
+class TestSpec extends WordSpec with MustMatchers {
+
+  "test" should {
+    "test function" in {
+      pending
+    }
+  }
+
+}
