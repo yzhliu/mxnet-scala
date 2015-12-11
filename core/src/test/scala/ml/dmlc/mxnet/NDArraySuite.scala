78e2dc5c94fefb4ffe0d7eebe1c2d736fe10700f
Yizhi Liu <javelinjs@gmail.com>
2015-12-11 14:22:33 +0800

    scala test supported

---
+++ core/src/test/scala/ml/dmlc/mxnet/NDArraySuite.scala
@@ -0,0 +1,9 @@
+package ml.dmlc.mxnet
+
+import org.scalatest.{FunSuite, BeforeAndAfterAll}
+
+class NDArraySuite extends FunSuite with BeforeAndAfterAll {
+  test("fake test") {
+    assert(1 == 1)
+  }
+}
